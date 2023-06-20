173a00011253d97cf26c018fcf7e8110c4194111
Alperen Aga <alperencode@gmail.com>
2023-06-20 13:35:53 +0300

    Fix similarity check bug Add ResizeImage to fix image size issues

--- FaceDetection/FaceDetection/Compare.cs
+++ FaceDetection/FaceDetection/Compare.cs
@@ -12,14 +12,18 @@ namespace FaceDetection
 
         public double CompareImages(Bitmap image1, Bitmap image2)
         {
-            int minWidth = Math.Min(image1.Width, image2.Width);
-            int minHeight = Math.Min(image1.Height, image2.Height);
+
+            int averageWidth = (image1.Width + image2.Width) / 2;
+            int averageHeigth = (image1.Height + image2.Height) / 2;
+
+            image1 = ResizeImage(image1, averageWidth, averageHeigth);
+            image2 = ResizeImage(image2, averageWidth, averageHeigth);
 
             double mse = 0;
 
-            for (int y = 0; y < minHeight; y++)
+            for (int y = 0; y < averageHeigth; y++)
             {
-                for (int x = 0; x < minWidth; x++)
+                for (int x = 0; x < averageWidth; x++)
                 {
                     Color pixel1 = image1.GetPixel(x, y);
                     Color pixel2 = image2.GetPixel(x, y);
@@ -32,11 +36,22 @@ namespace FaceDetection
                 }
             }
 
-            int totalPixels = minWidth * minHeight;
+            int totalPixels = averageWidth * averageHeigth;
             mse /= (totalPixels * 3);
 
-            return 100 - ((mse / (minHeight*minWidth)) * 100);
+            return 100 - (mse / totalPixels * 100);
             
+        }
+
+
+        private Bitmap ResizeImage(Bitmap image, int width, int height)
+        {
+            var resizedImage = new Bitmap(width, height);
+            using (var graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.DrawImage(image, 0, 0, width, height);
+            }
+            return resizedImage;
         }
 
     }
