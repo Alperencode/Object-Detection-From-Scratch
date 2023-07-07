f181a893028a8d4e951f26293e65085ed73e495c
Alperen Aga <alperencode@gmail.com>
2023-07-07 08:49:39 +0300

    Update CoinDotDetectionImproved Fix comment lines and clean the code

--- CoinDotDetectionImproved/CoinDotDetectionImproved/Form1.cs
+++ CoinDotDetectionImproved/CoinDotDetectionImproved/Form1.cs
@@ -25,57 +25,41 @@ namespace CoinDotDetectionImproved
             Color backgroundColor = methods.FindBackground(byteArr, byteArr.Length/10);
 
             int stride = byteArr.Length - (image.Height * image.Width * 3);
+
             // Convert image to black and white
             // 10 ms
             for (int i = 0; i < byteArr.Length - 2; i += 3)
             {
-                // If byte value is bigger than 100: set to min
+
+                // Making background black
+                // If byte value is bigger than background RGB: set next RGB to min
                 if (byteArr[i] >= backgroundColor.R && byteArr[i + 1] >= backgroundColor.G && byteArr[i + 2] >= backgroundColor.B)
-                {
                     for (int j = 0; j < 3; j++)
                         byteArr[i + j] = 0;
-                }
+
+                // Making coins white
+                // Else, set next RGB to max
                 else
-                {
                     for (int j = 0; j < 3; j++)
                         byteArr[i + j] = 255;
-                }
             }
 
-            /*
+            // Clearing out the pixels
+            // 85-90ms
             for (int i = 0; i < byteArr.Length - 2; i+=3)
             {
-                // If pixel is white and there is no black sequence, turn next sequence white and iterate through next sequence's pixel
+                // If the pixel is black but there is no black sequence, turn pixel to white
                 if (byteArr[i] == 0 && byteArr[i + 1] == 0 && byteArr[i + 2] == 0)
-                    if (!methods.BlackSequenceHorizontal(byteArr, i, 50))
+                    if (!methods.BlackSequenceHorizontal(byteArr, i, 20))
                         (byteArr[i], byteArr[i+1], byteArr[i+2]) = (255,255,255);
 
                 // If any of bytes are max but there is black sequence, set byte to min
-                if (byteArr[i] == 255 && byteArr[i + 1] == 255 && byteArr[i + 2] == 255)
-                    if (methods.BlackSequenceHorizontal(byteArr, i, 50))
+                if (byteArr[i] == 255 || byteArr[i + 1] == 255 || byteArr[i + 2] == 255)
+                    if (methods.BlackSequenceHorizontal(byteArr, i, 20))
                         (byteArr[i], byteArr[i + 1], byteArr[i + 2]) = (0, 0, 0);
             }
-            */
-
-            /*
-            int height = 0;
-            for (int i = 0; i < byteArr.Length - 2; i+=3)
-            {
-                if (byteArr[i] > 0 && byteArr[i + 1] > 0 && byteArr[i + 2] > 0)
-                {
-                    
-                    if (!methods.BlackSequenceVertical(byteArr, i, 30, image.Width))
-                    {
-                        // Found the coin
-                        height = i;
-                        break;
-                    }
-                }
-            }
-            */
 
             // AllocConsole();
-            // Console.WriteLine(height);
 
             // 2ms
             Bitmap newImage = methods.ByteToBitmap(byteArr, image.PixelFormat, image.Width, image.Height);
